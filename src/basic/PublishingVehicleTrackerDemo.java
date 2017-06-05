382faf91223d1dab2bc51b0e9fa0952f3e8d2f88
Chariemo <charleyschen@163.com>
2017-06-05 11:48:11 +0800

    publishing vehicle tracker

--- src/basic/PublishingVehicleTrackerDemo.java
+++ src/basic/PublishingVehicleTrackerDemo.java
@@ -1,8 +1,19 @@
 package basic;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Map;
+import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 import net.jcip.annotations.GuardedBy;
 import net.jcip.annotations.ThreadSafe;
@@ -12,31 +23,31 @@ public class PublishingVehicleTrackerDemo {
 
 	private final Map<String, SafePoint> locations;
 	private final Map<String, SafePoint> unmodifiableMap;
-	
+
 	public PublishingVehicleTrackerDemo(Map<String, SafePoint> locations) {
-		
+
 		this.locations = new ConcurrentHashMap<String, SafePoint>(locations);
 		this.unmodifiableMap = Collections.unmodifiableMap(this.locations);
 	}
-	
+
 	public Map<String, SafePoint> getLocations() {
-		
+
 		return unmodifiableMap;
 	}
-	
+
 	public SafePoint getLocation(String id) {
-	
+
 		return locations.get(id);
 	}
-	
+
 	public void setLocation(String id, int x, int y) {
-		
+
 		if (!locations.containsKey(id)) {
 			throw new IllegalArgumentException("invalid vehicle name: " + id);
 		}
 		locations.get(id).set(x, y);
 	}
-	
+
 	public static void main(String[] args) {
 		
 		
@@ -45,32 +56,33 @@ public class PublishingVehicleTrackerDemo {
 
 @ThreadSafe
 class SafePoint {
-	
-	@GuardedBy("this") private int x, y;
-	
-	private SafePoint(int []a) {
-		
+
+	@GuardedBy("this")
+	private int x, y;
+
+	private SafePoint(int[] a) {
+
 		this(a[0], a[1]);
 	}
-	
+
 	public SafePoint(SafePoint point) {
-		
+
 		this(point.get());
 	}
-	
+
 	public SafePoint(int x, int y) {
-		
+
 		this.x = x;
 		this.y = y;
 	}
-	
-	public synchronized int [] get() {
-		
-		return new int [] {x, y};
+
+	public synchronized int[] get() {
+
+		return new int[] { x, y };
 	}
-	
+
 	public synchronized void set(int x, int y) {
-		
+
 		this.x = x;
 		this.y = y;
 	}
